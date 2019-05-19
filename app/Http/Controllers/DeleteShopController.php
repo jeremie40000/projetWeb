a4766d949e85e4c408d494e0368a1bb46e4e147d
jeremie40000 <jeremie40000@gmail.com>
2019-05-20 00:19:37 +0200

    fixing delete shop

--- app/Http/Controllers/DeleteShopController.php
+++ app/Http/Controllers/DeleteShopController.php
@@ -9,7 +9,24 @@ use Illuminate\Support\Facades\DB;
 class DeleteShopController extends Controller
 {
     public function index(Request $req){
-      DB::table('shop')->where('siret',$req->input('inputSiret'))->delete();
+      $siret = $req->input('inputSiret');
+      $menus = DB::select("select idm from menu where idshop='$siret'");
+      $hours = DB::select("select idhours from openingschedule where idshop='$siret'");
+      $menuarray = [];
+      foreach ($menus as $m) {
+        array_push($menuarray,$m->idm);
+      }
+      $hoursarray = [];
+      foreach ($hours as $h) {
+        array_push($hoursarray,$h->idhours);
+      }
+
+      DB::table('dish')->whereIn('idmenu',$menuarray)->delete();
+      DB::table('menu')->where('idshop',$siret)->delete();
+      DB::table('openingschedule')->where('idshop',$siret)->delete();
+      DB::table('openingtime')->whereIn('idh', $hoursarray)->delete();
+      DB::table('image')->where('ids',$siret)->delete();
+      DB::table('shop')->where('siret',$siret)->delete();
       $user = Auth::user();
       $req = "select * from shop where idm='$user->id'";
     	$shops = DB::select($req);
